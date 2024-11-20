04839b78ff460b0035fe912e7083de5e37b75165
Johan Kremer <johan.h.kremer@gmail.com>
2024-11-20 08:23:03 +0100

    Initiated Prisma and migrated paint schema to DB

---
+++ prisma/migrations/20241119185657_removed_color/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `color` on the `Paint` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Paint` DROP COLUMN `color`;
