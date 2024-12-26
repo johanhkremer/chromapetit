f2307d70ecc47ffd91b8471ce7b74be1a0004ad9
Johan Kremer <johan.h.kremer@gmail.com>
2024-12-27 00:54:52 +0100

    Removed dynamic route just to see if deploy will work

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,35 +1,35 @@
-import { FC } from 'react';
-import { toast } from 'sonner';
-import ProjectDetail from './components/project-detail';
-import getProjectById from '@/app/actions/projects/getProjectById';
-import Link from 'next/link';
+// import { FC } from 'react';
+// import { toast } from 'sonner';
+// import ProjectDetail from './components/project-detail';
+// import getProjectById from '@/app/actions/projects/getProjectById';
+// import Link from 'next/link';
 
-interface PageProps {
-    params: { id: string };
-}
+// interface PageProps {
+//     params: { id: string };
+// }
 
-const ProjectPage: FC<PageProps> = async ({ params }) => {
-    let project;
-    try {
-        project = await getProjectById(params.id);
-    } catch (error) {
-        toast.error(`Something went wrong: ${(error as Error).message}`);
-        return (
-            <section>
-                <h1>Project</h1>
-                <p>Something went wrong. Please try again later.</p>
-                <Link href="/projects" className="hover:text-opacity-50">
-                    Back to projects
-                </Link>
-            </section>
-        );
-    }
+// const ProjectPage: FC<PageProps> = async ({ params }) => {
+//     let project;
+//     try {
+//         project = await getProjectById(params.id);
+//     } catch (error) {
+//         toast.error(`Something went wrong: ${(error as Error).message}`);
+//         return (
+//             <section>
+//                 <h1>Project</h1>
+//                 <p>Something went wrong. Please try again later.</p>
+//                 <Link href="/projects" className="hover:text-opacity-50">
+//                     Back to projects
+//                 </Link>
+//             </section>
+//         );
+//     }
 
-    return (
-        <article>
-            <ProjectDetail project={project} />
-        </article>
-    );
-};
+//     return (
+//         <article>
+//             <ProjectDetail project={project} />
+//         </article>
+//     );
+// };
 
-export default ProjectPage;
+// export default ProjectPage;
