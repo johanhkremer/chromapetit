b37f26ae77bdbf8a46a8764663d13616dfdc18d5
Johan Kremer <johan.h.kremer@gmail.com>
2024-12-28 17:25:50 +0100

    Updated changes

--- app/actions/paints/getPaints.ts
+++ app/actions/paints/getPaints.ts
@@ -1,14 +1,20 @@
 "use server";
 
+export const dynamic = 'force-dynamic';
+
 import { prisma } from "@/prisma";
 
 export const getPaints = async () => {
     try {
         const paints = await prisma.paint.findMany();
-        console.log("Paints:", paints);
+        console.log("Paints fetched:", paints);
         return paints;
-    } catch (error) {
-        console.error("Error fetching paints:", error);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error("Error fetching paints:", error.message);
+        } else {
+            console.error("Error fetching paints:", error);
+        }
         throw new Error("Failed to fetch paints.");
     }
 };
