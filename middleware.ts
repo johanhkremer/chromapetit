5354281f1b8cd4b6246d7f94c2c393ad89ffc62e
Johan Kremer <johan.h.kremer@gmail.com>
2024-12-28 11:10:28 +0100

    Changed .env

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+    const token = await getToken({ req, secret: process.env.AUTH_SECRET })
 
     if (!token && req.nextUrl.pathname.startsWith("/projects")) {
         const loginUrl = new URL("/auth/login", req.nextUrl.origin)
