05735eb18c47454757655737afc04b2d4682a02f
Johan Kremer <johan.h.kremer@gmail.com>
2024-12-29 15:07:35 +0100

    Updated changes

--- components/sidebar/nav-projects.tsx
+++ components/sidebar/nav-projects.tsx
@@ -46,6 +46,7 @@ export function NavProjects() {
 
     console.log("Projects:", projects)
     console.log("session:", session)
+    console.log("user:", session?.user)
 
     useEffect(() => {
         if (session) {
@@ -111,7 +112,7 @@ export function NavProjects() {
                             <div className="flex justify-center">
                                 <LoadSpinner />
                             </div>
-                        ) : !session ? (
+                        ) : !session?.user ? (
                             <SidebarMenuItem>
                                 <SidebarMenuButton asChild className="text-sidebar-foreground/70">
                                     <Link href="/auth/login" className="flex items-center">
