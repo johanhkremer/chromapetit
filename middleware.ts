65d06c148cd884b44d5c2b9ddfcea90b65d37503
Johan Kremer <johan.h.kremer@gmail.com>
2024-12-30 00:07:18 +0100

    Trying to add middleware again

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
-// import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function middleware() {
-    // const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req, secret: process.env.AUTH_SECRET })
 
-    // if (!token && req.nextUrl.pathname.startsWith("/projects")) {
-    //     const loginUrl = new URL("/auth/login", req.nextUrl.origin)
-    //     return NextResponse.redirect(loginUrl)
-    // }
+    if (!token && req.nextUrl.pathname.startsWith("/projects")) {
+        const loginUrl = new URL("/auth/login", req.nextUrl.origin)
+        return NextResponse.redirect(loginUrl)
+    }
 
-    // if (req.nextUrl.pathname.startsWith("/auth")) {
-    //     return NextResponse.next();
-    // }
+    if (req.nextUrl.pathname.startsWith("/auth")) {
+        return NextResponse.next();
+    }
 
     return NextResponse.next()
 }
 
 export const config = {
     matcher: [
-        // "/projects(/:path*)",
+        "/projects(/:path*)",
     ],
 }
